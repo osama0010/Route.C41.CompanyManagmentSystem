cc37f1670946d2f966fa94ca4ba2678e8cbaf963
osama0010 <osama.00.mail@gmail.com>
2024-05-05 04:42:08 +0300

    Refactor DBcontext with Dl + connectionString in appsetting + apply migrations

--- CompanyManagmentSystem.DAL/Data/Configurations/DepartmentConfiguration.cs
+++ CompanyManagmentSystem.DAL/Data/Configurations/DepartmentConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-namespace CompanyManagmentSystem.DAL.Models
+using CompanyManagmentSystem.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
